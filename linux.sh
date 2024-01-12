6cc0bcc40bdfa820ffb17d501ff6b8c05232e67f
purple_flood <49845374+PurpleFlood@users.noreply.github.com>
2024-01-12 23:03:05 +0100

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt update \
-& sudo apt install spice-vdagent qemu-guest-agent qemu-guest-agentspice-webdavd -y ; reboot
+& sudo apt install spice-vdagent qemu-guest-agent qemu-guest-agent spice-webdavd -y ; reboot
